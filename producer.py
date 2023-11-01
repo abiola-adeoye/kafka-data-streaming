e3de0e4cfac54ed19e3d63c14e785d6d6d14bc59
abiola-adeoye <slmadeoye@gmail.com>
2023-11-01 10:05:27 +0100

    finnhub api fully implemented, docstrings to be added in future

--- producer.py
+++ producer.py
@@ -1,5 +1,6 @@
 from src.finnhub_helper import FinnhubTradeAPI
 
+# add a feature to collect symbol input or use default symbols
 symbols = ['BINANCE:BTCUSDT', 'BINANCE:ETHUSDT', 'BINANCE:XRPUSDT', 'BINANCE:DOGEUSDT']
-prod = FinnhubTradeAPI()
-prod.on_open()
+prod = FinnhubTradeAPI(symbols=symbols, test_mode=False)
+prod.start_stream()
--- src/finnhub_helper.py
+++ src/finnhub_helper.py
@@ -1,3 +1,4 @@
+# add docstrings to the code
 import os
 import json
 
@@ -11,35 +12,39 @@ class FinnhubTradeAPI:
     _FINNHUB_WS_ADDRESS = "wss://ws.finnhub.io"
     _SUBSCRIPTION_MSG = '{{"type":"subscribe","symbol":"{symbol_abv}"}}'
 
-    def __init__(self, symbols: list = None):
+    def __init__(self, symbols: list = None, test_mode=True):
         # setup logger for class
         self.logger = load_logging(__class__.__name__)
 
         load_dotenv()
+        self.test_mode = test_mode
         self.token = os.getenv('FINNHUB_API')
         self.symbols = symbols      # the symbols we're getting data for e.g. AMZN
 
-    def start_stream(self):
+    def start_stream(self) -> None:
+        if self.test_mode:
+            websocket.enableTrace(True)
         finnhub_ws = websocket.WebSocketApp(f"{self._FINNHUB_WS_ADDRESS}?token={self.token}",
+                                            on_open=self.on_open,
                                             on_message=self.on_message,
                                             on_error=self.on_error,
                                             on_close=self.on_close)
-        finnhub_ws.on_open = self.on_open
         finnhub_ws.run_forever()
 
-    def on_open(self, ws):
+    def on_open(self, ws: websocket) -> None:
         for name in self.symbols:
             payload = self._SUBSCRIPTION_MSG.format(symbol_abv=name)
             ws.send(payload)
+            self.logger.info(f"Subscribed to {name} symbol")
 
     @staticmethod
-    def on_message(ws, message):
+    def on_message(ws, message: str):
         response = json.loads(message)
         response_data = response['data']
         print(response_data)
 
     def on_error(self, ws, error):
-        self.logger.error(msg="Error while streaming", exc_info=error)  # this is not going to work
+        self.logger.error(msg=f"Error while streaming: {error}", exc_info=1)
 
     def on_close(self, ws, close_status, close_message):
-        pass
+        self.logger.info(msg=f"Data stream is closing...")
